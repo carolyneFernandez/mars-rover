aa0a8976c1c22649ada00654e71f135f66bb5fdc
Florian <flodu141@gmail.com>
2019-10-17 19:18:06 +0200

    suppr id classes rover

--- src/Entity/EcoRover.php
+++ src/Entity/EcoRover.php
@@ -4,22 +4,10 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity(repositoryClass="App\Repository\EcoRoverRepository")
- */
+
 class EcoRover extends Rover
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 
     /**
      * Algorithme qui choisira le prochain coup en fonction de son type de rover
--- src/Entity/IntelligentRover.php
+++ src/Entity/IntelligentRover.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 class IntelligentRover extends Rover
 {
-    
+
     /**
      * Algorithme qui choisira le prochain coup en fonction de son type de rover
      */
