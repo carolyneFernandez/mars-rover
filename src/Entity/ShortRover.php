d3812c25187a6db0601e34511a2074de280ca51b
carolyne <carolyne@MC-G-LPCM-C120-12.local>
2019-11-22 14:35:35 +0100

    modification

--- src/Entity/ShortRover.php
+++ src/Entity/ShortRover.php
@@ -262,6 +262,92 @@ class ShortRover extends Rover
         return $tab;
     }
 
+
+
+
+
+
+    public function brensenham($posX, $posY, $destX, $destY, $direction = false, $turn = false){
+        $x = $posX;
+        $y = $posY;
+        $dx = $destX - $posX; //distance sur l'axe des abscisses
+        $dy = $destY - $posY; //distance sur l'axe des ordonnees
+        //direction du segment
+        $xinc = $dx > 0 ? 1 : -1; 
+        $yinc = $dy > 0 ? 1 : -1; 
+        //              |
+        //              |
+        //   xinc: -1   |   xinc: 1
+        //   yinc: 1    |   yinc: 1
+        //              |
+        // --------------------------------             
+        //              |
+        //   xinc: -1   |   xinc: 1
+        //   yinc: -1   |   yinc: -1
+        //              |
+        //              |
+        
+        //convertion en valeur absolue pour evaluer la pente du segment
+        $dx = abs($dx);
+        $dy = abs($dy);
+        
+        //selon la pente du segment
+        if($dx > $dy){ //Si est plutot horizontale
+            $error = $dx / 2;
+            for ($i=1; $i <= $dx; $i++) { //pour chaque pixel sur la distance des absisses
+                $x += $xinc;
+                $error += $dy;
+                if($error >= $dx){
+                    $error -= $dx;
+                    $y += $yinc;
+                }
+                
+                if ($turn) {
+                    $path[$i][$y][$x] = true;
+                } else {
+                    $path[$y][$x] = true;
+                }
+                //si on cherche une direction
+                if($direction == true && ($y-1 >= 0 && $y+1 <= 9)){
+                    if ($turn) {
+                        $path[$i][$y-1][$x] = true;
+                        $path[$i][$y+1][$x] = true;
+                    }
+                    $path[$y-1][$x] = true;
+                    $path[$y+1][$x] = true;
+                }
+            }
+        } else{ //Si est plutot verticale
+            $error = $dy / 2;
+            for ($i=1; $i <= $dy; $i++) { 
+                $y += $yinc;
+                $error += $dx;
+                if($error >= $dy){
+                    $error -= $dy;
+                    $x += $xinc;
+                }
+                if ($turn) {
+                    $path[$i][$y][$x] = true;
+                } else {
+                    $path[$y][$x] = true;
+                }
+                //si on cherche une direction
+                if($direction == true && ($x-1 >= 0 && $x+1 <= 9)){
+                    if ($turn) {
+                        $path[$i][$y][$x-1] = true;
+                        $path[$i][$y][$x+1] = true;
+                    } 
+                    $path[$y][$x-1] = true;
+                    $path[$y][$x+1] = true;
+                }
+            }   
+        }
+
+
+
+        return $path;
+    }
+
     
     
 }
