dd93ac5fa4f2f8f1c3a0666b68f379b243d21c46
Alexandre LASCAUX <43177435+Smirkov@users.noreply.github.com>
2019-11-23 12:03:20 +0100

    Click sur différent choix et ajout de classe css

--- src/main.js
+++ src/main.js
@@ -1,17 +1,35 @@
-document.addEventListener('DOMContentLoaded', run, false);
-
-function run() {
-
-}
-
 function selectedButton(id) {
-    console.log('passed1');
-    let buttonSelect = document.querySelectorAll('.buttonSelect-row')
+    const buttonSelect = document.querySelectorAll('.buttonSelect-row');
+    const svg = document.querySelectorAll('.logoSelect');
     if(!buttonSelect[id].classList.contains('logoSelected')){
-        
-        console.log('passed2');
         buttonSelect[id].classList.add('logoSelected');
     } else {
         buttonSelect[id].classList.remove('logoSelected');
+    }
+    if(!svg[id].classList.contains('logoSelectedFill') && id === 0){
+        svg[id].classList.add('logoSelectedFill');
+        svg[id+1].classList.add('logoSelectedFill');
+    } else if (!svg[id+1].classList.contains('logoSelectedFill') && id === 1) {
+        svg[id+1].classList.add('logoSelectedFill');
+        svg[id+2].classList.add('logoSelectedFill');
+    } else if (svg[id].classList.contains('logoSelectedFill') && id === 0) {
+        svg[id].classList.remove('logoSelectedFill');
+        svg[id+1].classList.remove('logoSelectedFill');
+    } else if (svg[id+1].classList.contains('logoSelectedFill') && id === 1) {
+        svg[id+1].classList.remove('logoSelectedFill');
+        svg[id+2].classList.remove('logoSelectedFill');
+    } 
+    if (id === 0) {
+        if (buttonSelect[id+1].classList.contains('logoSelected')){
+            buttonSelect[id+1].classList.remove('logoSelected');
+            svg[id+2].classList.remove('logoSelectedFill');
+            svg[id+3].classList.remove('logoSelectedFill');
+        }
+    } else if (id === 1) {
+        if (buttonSelect[id-1].classList.contains('logoSelected')){
+            buttonSelect[id-1].classList.remove('logoSelected');
+            svg[id-1].classList.remove('logoSelectedFill');
+            svg[id].classList.remove('logoSelectedFill');
+        }
     }
 }
