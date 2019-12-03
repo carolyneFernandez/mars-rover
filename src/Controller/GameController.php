93f509efbb72a2c09fcda8f8c6567499214c0500
Florian <flodu141@gmail.com>
2019-12-03 19:45:40 +0100

    reponse API front

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\EcoRover;
 use App\Entity\IntelligentRover;
+use App\Entity\ShortRover;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class GameController extends AbstractController
 {
@@ -48,14 +53,16 @@ class GameController extends AbstractController
      */
     public function index(Request $request)
     {
-        $rover = new IntelligentRover();
+//        $rover = new IntelligentRover();
+        $errors = array();
         /*
-         * Requête API méthode GET :
+         * Requête du front vers l'API avec la méthode GET :
          * int posX,
          * int posY,
          * string typeRover (short, intelligent, economic)
          * int energy
          * string map (nom de la map à utiliser)
+         * http://localhost:8000/game?posX=2&posY=4&typeRover=intelligent&energy=321&map=23144141212.json
          */
         /*
          * Réponse de la requête API :
@@ -65,13 +72,99 @@ class GameController extends AbstractController
          * array analyse ( ['x,y','x,y'] -> implicitera le fait qu'on procède à des analyses de sols ).
          * array action ( ['move','stay', ... ] -> la décision du rover )
          */
+        if ($request->isMethod('GET')) {
+            if ($request->get('typeRover')) {
+                switch ($request->get('typeRover')) {
+                    case 'short':
+                        $rover = new ShortRover();
+                        break;
+                    case 'intelligent':
+                        $rover = new IntelligentRover();
+                        break;
+                    case 'economic':
+                        $rover = new EcoRover();
+                        break;
+                    default:
+                        $errors[] = "Le type de rover n'existe pas.";
+                }
+            } else {
+                $errors[] = "Le type de rover n'est pas renseigné.";
 
-        $typeRover = "intelligent";
-        $result = $rover->choiceStep();
+            }
+            if (($request->get('posX') != null && $request->get('posY') != null)) {
+                $rover->setPosX($request->get('posX'))->setPosY($request->get('posY'));
+//                $rover->setPosZ($rover->requestGetZ($rover->getPosX(), $rover->getPosY()) );
+            } else {
+                $errors[] = "La position du rover n'est pas renseignée.";
+            }
+            if ($request->get('energy')) {
+                $rover->setEnergy($request->get('energy'));
+            } else {
+                $errors[] = "L'énergie du rover n'est pas renseignée.";
+            }
+            if($request->get('map')){
+                $map = $request->get('map');
+            }else{
+                $errors[] = "La carte n'a pas été passée.";
+            }
+
+            
+
+            $result = $rover->choiceStep();
+        }
+
+        $result['errors'] = $errors;
         $response = json_encode($result);
 
-        return $response;
+        return new Response($response);
 //        return $this->render('game/index.html.twig', ['controller_name' => 'GameController',]);
     }
+
+
+    /**
+     * Non fonctionnel
+     * Utiliser plutôt : http://localhost:8002/game?posX=2&posY=4&typeRover=intelligent&energy=321&destX=8&destY=5&map=jsonmap.json&
+     * @Route("/simulate/request/rover", name="simule_request_rover")
+     * @return string
+     * @throws TransportExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     */
+    public function simuleRequete()
+    {
+
+
+        $data = [
+            'posX' => 2,
+            'posY' => 4,
+            'typeRover' => 'intelligent',
+            'energy' => 321,
+            'destX' => 8,
+            'destY' => 5,
+            'map' => 'jsonmap.json'
+        ];
+        $dataChaine = "?";
+        foreach ($data as $key=>$value){
+            $dataChaine .= $key."=".$value."&";
+        }
+
+//
+        $httpClient = HttpClient::create();
+
+        $response = $httpClient->request('GET', $_ENV['URL_API_ROVER'].$dataChaine);
+
+        echo $response->getContent();
+
+
+
+
+        dump($response);
+
+        return $this->renderView('game/index.html.twig');
+
+
+    }
+
 
 }
