ff35cccb6aa65fd496d210d0d7cfbccc062cd5ea
Florian <flodu141@gmail.com>
2019-12-10 15:02:19 +0100

    fix button CHOOSE

--- src/selectRovers/selectRovers.js
+++ src/selectRovers/selectRovers.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', getRovers, false);
 
 function getRovers(){
-    const request = new Request('http://localhost:80/rovers.json');
+
+
+    // loadJSON(function(response) {
+    //     // Parse JSON string into object
+    //     let data = JSON.parse(response);
+    //     console.log(data);
+    //     for(let i = 0; i < data.length; i++){
+    //         createRover(data[i].roverName, data[i].type, data[i].pathImage);
+    //     }
+    // });
+
+    const request = new Request('Front/DumpDatas/rovers.json');
 
     fetch(request)
         .then(function(response) { return response.json(); })
@@ -16,9 +27,9 @@ function getRoversSelected(){
     let rovers = document.getElementsByClassName('roverSelected');
     console.log(rovers);
     let roversSelected = [];
-        
+
     for(let i = 0; i < rovers.length; i++){
-        roversSelected.push(new roverObject(rovers[i].attributes.name.value, rovers[i].attributes.value.value));
+        roversSelected.push(new roverObject(rovers[i].parentElement.attributes.name.value, rovers[i].parentElement.attributes.value.value));
     }
     window.localStorage.setItem('roversSelected', JSON.stringify(roversSelected));
 
@@ -71,4 +82,18 @@ function selectedRover(e){
     } else {
         e.target.classList.remove('roverSelected');
     }
+}
+
+function loadJSON(callback) {
+
+    let xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'Front/DumpDatas/rovers.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
 }
